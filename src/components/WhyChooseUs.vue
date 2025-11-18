7984d3190b85b37d71fb2770a4114700cc2d562f
lisaaly9 <lisatasya175@gmail.com>
2025-11-18 11:54:41 +0800

    Nambah Component WhyChooseUs

---
+++ src/components/WhyChooseUs.vue
@@ -0,0 +1,32 @@
+<template>
+    <section 
+    id="features"
+    class="px-6 py-16 md:py-20 bg-[#E0CBA8]">
+    <div class="max-w-4xl mx-auto text-center mb-20">
+        <h2 class="text-3xl md:text-4xl font-bold text-[#5B3A29]">
+        “Kenapa Harus Pilih Catering Ini?”
+        </h2>
+    </div>
+
+    <div class="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+
+        <!-- Card 1 -->
+        <div class="bg-white/40 rounded-xl shadow-md p-8 text-center">
+        <div class="text-5xl mb-4">🕒</div>
+        <p class="text-[#5B3A29] font-semibold text-lg">Tepat Waktu</p>
+        </div>
+
+        <!-- Card 2 -->
+        <div class="bg-white/40 rounded-xl shadow-md p-8 text-center">
+        <div class="text-5xl mb-4">🍽️</div>
+        <p class="text-[#5B3A29] font-semibold text-lg">Rasa Lezat</p>
+        </div>
+
+        <!-- Card 3 -->
+        <div class="bg-white/40 rounded-xl shadow-md p-8 text-center">
+        <div class="text-5xl mb-4">💵</div>
+        <p class="text-[#5B3A29] font-semibold text-lg">Harga Bersahabat</p>
+        </div>
+    </div>
+    </section>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Navbar from '../components/Navbar.vue'
 import HeroSection from '../components/HeroSection.vue'
+import WhyChooseUs from '@/components/WhyChooseUs.vue';
 import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
   <Navbar/>
   <HeroSection/>
+  <WhyChooseUs/>
   <Footer/>
 </template>
 
